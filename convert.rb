8903142372c4b6573b9de00d37d905d792c6bc5a
Piotr Żurek (Coders'Peak) <piotr@coderspeak.com>
2018-04-10 11:36:42 +0200

    Flush the log file after write, update .gitignore

--- convert.rb
+++ convert.rb
@@ -21,6 +21,7 @@ puts 'Migrating data...'
 
 error_log = File.new('errors.log', 'a')
 error_log.puts "--- Conversion started #{Time.now.to_s} ---"
+error_log.flush
 
 begin
   map_iter = 1
@@ -59,6 +60,7 @@ begin
         rescue PG::Error => e
           puts "Encountered PG::Error: #{e.to_s}. Trying next result..."
           error_log.puts "INSERT: PG::Error: #{e.to_s}"
+          error_log.flush
           next
         end
         result_iter += 1
@@ -67,6 +69,7 @@ begin
     rescue PG::Error => e
       puts "Encountered PG::Error: #{e.to_s}. Trying next definition..."
       error_log.puts "SELECT: PG::Error: #{e.to_s}"
+      error_log.flush
       next
     end
     map_iter += 1
@@ -76,6 +79,7 @@ rescue => e
   puts e.backtrace
   error_log.puts "Exception: #{e.class} - #{e}"
   error_log.puts e.backtrace
+  error_log.flush
   exit_value = 1
 end
 
@@ -88,6 +92,7 @@ puts 'Closing source connection...'
 source_connection.finish
 
 error_log.puts "--- Conversion finished #{Time.now.to_s} ---"
+error_log.flush
 
 exit exit_value
 
