3015236fca79de2e32e38e5911851bec7035edb3
Piotr Żurek (Coders'Peak) <piotr@coderspeak.com>
2018-04-10 11:33:03 +0200

    Add error log and progress indication

--- convert.rb
+++ convert.rb
@@ -19,13 +19,20 @@ dest_connection = PG::Connection.new(host: @config['destination']['hostname'], p
 
 puts 'Migrating data...'
 
+error_log = File.new('errors.log', 'a')
+error_log.puts "--- Conversion started #{Time.now.to_s} ---"
+
 begin
+  map_iter = 1
+  map_count = @config['map'].count
   @config['map'].each do |tablemap|
     begin
       src_table = tablemap['source_table']
       dest_table = tablemap['destination_table']
-      puts "SELECT * FROM #{src_table}"
+      puts "(#{map_iter}/#{map_count}) SELECT * FROM #{src_table}"
       results = source_connection.exec("SELECT * FROM #{src_table}")
+      result_iter = 1
+      result_count = results.count
       results.each do |result|
         begin
           values = []
@@ -47,22 +54,28 @@ begin
               query += "$#{i})"
             end
           end
-          puts "Query: (#{query}), values: (#{values})"
+          puts "(#{result_iter}/#{result_count}) Query: (#{query}), values: (#{values})"
           dest_connection.exec_params(query, values)
         rescue PG::Error => e
           puts "Encountered PG::Error: #{e.to_s}. Trying next result..."
+          error_log.puts "INSERT: PG::Error: #{e.to_s}"
           next
         end
+        result_iter += 1
       end
-      puts "Migrated from #{src_table} to #{dest_table}!"
+      puts "(#{map_iter}/#{map_count}) Migrated from #{src_table} to #{dest_table}!"
     rescue PG::Error => e
       puts "Encountered PG::Error: #{e.to_s}. Trying next definition..."
+      error_log.puts "SELECT: PG::Error: #{e.to_s}"
       next
     end
+    map_iter += 1
   end
 rescue => e
   puts "Encountered exception: #{e.class} - #{e}"
   puts e.backtrace
+  error_log.puts "Exception: #{e.class} - #{e}"
+  error_log.puts e.backtrace
   exit_value = 1
 end
 
@@ -73,6 +86,8 @@ dest_connection.finish
 puts 'Closing source connection...'
 
 source_connection.finish
+
+error_log.puts "--- Conversion finished #{Time.now.to_s} ---"
 
 exit exit_value
 
